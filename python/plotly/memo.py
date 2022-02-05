8927aa36d74086d0f778011fca509066545ded4e
RyomaKaneko <banzai.nippon2030@gmail.com>
2022-02-06 06:49:12 +0900

    add: Interactive operation インタラクティブな操作

--- python/plotly/memo.py
+++ python/plotly/memo.py
@@ -71,3 +71,26 @@ fig2.show()
 fig3 = go.Figure()
 fig3.add_traces([scatter,bar])
 fig3.show()
+
+# Interactive operation
+# インタラクティブな操作
+import plotly.graph_objects as go
+data1 = {
+    "x": [1,2,3],
+    "y": [1,5,3],
+    "hovertext": ["A社","B社","C社"],
+    "name": "bar"
+}
+scatter = go.Scatter(
+    x=data1['x'],y=data1['y'],
+    hovertext=data1['hovertext'],
+    name=data1['name'])
+data2 = {
+    "x": [1,2,3],
+    "y": [1,2,3],
+    "name": "line"
+}
+bar = go.Bar(x=data2['x'],y=data2['y'],name=data2['name'])
+fig3 = go.Figure()
+fig3.add_traces([scatter,bar])
+fig3.show()
